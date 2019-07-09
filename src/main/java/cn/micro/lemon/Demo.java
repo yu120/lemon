7bc41f7033ab603df955419bd5c9377acfea9c4c
lry <595208882@qq.com>
2019-07-09 13:56:22 +0800

    init

--- src/main/java/cn/micro/lemon/Demo.java
+++ src/main/java/cn/micro/lemon/Demo.java
@@ -1,22 +1,37 @@
 package cn.micro.lemon;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Demo {
 
     public static void main(String[] args) {
         String interfaceClass = "cn.micro.biz.dubbo.provider.DemoService";
-        String methodName = "sayHello";
-        List<String> paramTypes = new ArrayList<>();
-        paramTypes.add("java.lang.String");
-        List<Object> paramValues = new ArrayList<>();
-        paramValues.add("张三");
 
         DubboInvokeProxy dubboInvokeProxy = new DubboInvokeProxy();
         dubboInvokeProxy.initialize("micro-dubbo-gateway", "zookeeper://127.0.0.1:2181");
-        Object result = dubboInvokeProxy.invoke(interfaceClass, methodName, paramTypes, paramValues);
-        System.out.println(result);
+        Object result1 = dubboInvokeProxy.invoke(interfaceClass, "sayHello",
+                Arrays.asList("java.lang.String"),
+                Arrays.asList("张三"));
+        System.out.println("sayHello: " + result1);
+
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("name", "张三");
+        map2.put("age", 22);
+        Object result2 = dubboInvokeProxy.invoke(interfaceClass, "test",
+                Arrays.asList("cn.micro.biz.dubbo.provider.User"),
+                Arrays.asList(map2));
+        System.out.println("test: " + result2);
+
+        Map<String, Object> map3 = new HashMap<>();
+        map3.put("name", "李四");
+        map3.put("age", 33);
+        List<Object> list3=new ArrayList<>();
+        list3.add(map2);
+        list3.add(map3);
+        Object result3 = dubboInvokeProxy.invoke(interfaceClass, "demo",
+                Arrays.asList("java.util.List"),
+                Arrays.asList(list3));
+        System.out.println("demo: " + result3);
     }
 
 }
