bb1ee2a83f9c0e3c30d5ef5133273797e51a6f7b
lry <595208882@qq.com>
2019-07-10 14:14:24 +0800

    init

---
+++ src/main/java/cn/micro/lemon/server/LemonServer.java
@@ -0,0 +1,63 @@
+package cn.micro.lemon.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.URL;
+import org.apache.log4j.lf5.LogLevel;
+
+import java.nio.channels.SocketChannel;
+
+@Slf4j
+public class LemonServer {
+
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+    private ServerBootstrap serverBootstrap;
+    private ChannelFuture channelFuture;
+
+    public static void main(String[] args) {
+        URL url = URL.valueOf("http://127.0.0.1:9001");
+        LemonServer mioServer = new LemonServer();
+        mioServer.initialize(url);
+    }
+
+    public void initialize(URL url) {
+        try {
+            bossGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup();
+            serverBootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new HttpRequestDecoder());
+                            ch.pipeline().addLast(new HttpResponseEncoder());
+                            // Convert multiple requests from HTTP to FullHttpRequest/FullHttpResponse
+                            ch.pipeline().addLast(new HttpObjectAggregator(Integer.MAX_VALUE));
+                            ch.pipeline().addLast(new LemonServerHandler());
+                        }
+                    });
+            channelFuture = serverBootstrap.bind(url.getPort()).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error("The start server is fail", e);
+        }
+    }
+
+    public void destroy() {
+        try {
+            if (bossGroup != null) {
+                bossGroup.shutdownGracefully();
+            }
+            if (workerGroup != null) {
+                workerGroup.shutdownGracefully();
+            }
+        } catch (Exception e) {
+            log.error("The destroy server is fail", e);
+        }
+    }
+
+}
