b416b1cf61742ca37b3eadd965ec9eab494d715a
lry <595208882@qq.com>
2019-07-11 13:15:59 +0800

    init

--- src/main/java/cn/micro/lemon/filter/support/LemonInvokeFilter.java
+++ src/main/java/cn/micro/lemon/filter/support/LemonInvokeFilter.java
@@ -6,6 +6,7 @@ import cn.micro.lemon.dubbo.DubboLemonInvoke;
 import cn.micro.lemon.filter.IFilter;
 import cn.micro.lemon.filter.LemonChain;
 import cn.micro.lemon.filter.LemonContext;
+import lombok.extern.slf4j.Slf4j;
 import org.micro.neural.extension.Extension;
 
 import java.util.concurrent.CompletableFuture;
@@ -15,6 +16,7 @@ import java.util.concurrent.CompletableFuture;
  *
  * @author lry
  */
+@Slf4j
 @Extension(order = 100)
 public class LemonInvokeFilter implements IFilter {
 
@@ -29,9 +31,24 @@ public class LemonInvokeFilter implements IFilter {
     @Override
     public void doFilter(LemonChain chain, LemonContext context) throws Throwable {
         CompletableFuture<Object> future = lemonInvoke.invokeAsync(context);
-        if (future != null) {
-            future.whenComplete((result, t) -> context.writeAndFlush(result));
+        if (future == null) {
+            log.error("The completable future is null by context:{}", context);
+            return;
         }
+
+        future.whenComplete((result, throwable) -> {
+            if (throwable != null) {
+                log.error(throwable.getMessage(), throwable);
+            }
+
+            try {
+                chain.doFilter(context);
+            } catch (Throwable t) {
+                log.error(t.getMessage(), t);
+            } finally {
+                context.writeAndFlush(result);
+            }
+        });
     }
 
     @Override
