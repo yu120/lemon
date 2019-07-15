c69ff3cb431976791875b231a92cfa7a11647312
lry <595208882@qq.com>
2019-07-15 14:12:38 +0800

    Restructure

--- src/test/java/cn.micro.lemon/GenericInvokeDemo.java
+++ src/test/java/cn.micro.lemon/GenericInvokeDemo.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Demo {
+public class GenericInvokeDemo {
 
     public static void main(String[] args) {
         RegistryConfig registryConfig = new RegistryConfig();
